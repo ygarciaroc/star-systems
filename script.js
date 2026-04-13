5fdfeb2f1be0d176873273e90cc2843976127d17
Yammz <113056357+ygarciaroc@users.noreply.github.com>
2026-04-13 12:53:40 -0600

    Create script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+// Mensaje de bienvenida en la consola para otros desarrolladores
+console.log("%c Star Systems | Inicializado ", "color: #c0c0c0; background: #222; font-size: 15px; padding: 5px;");
+
+// Aquí podrías añadir un efecto de sonido tipo "click" industrial 
+// o transiciones suaves entre secciones.
